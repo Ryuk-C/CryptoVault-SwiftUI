e4006b0d5c0436e7d234ecf7e89f8a37448c37e3
Ryuk-C <chaznedar@edu.cdv.pl>
2023-04-21 11:17:11 -0700

    Initial Commit

---
+++ CryptoVault/CryptoVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoVaultApp.swift
+//  CryptoVault
+//
+//  Created by Cuma Haznedar on 21/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptoVaultUITests/CryptoVaultUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CryptoVaultUITestsLaunchTests.swift
+//  CryptoVaultUITests
+//
+//  Created by Cuma Haznedar on 21/04/2023.
+//
+
+import XCTest
+
+final class CryptoVaultUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
