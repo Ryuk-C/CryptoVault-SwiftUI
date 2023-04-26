c06eeb9a8f2d19c627123dad4436bb8f74f43105
Ryuk-C <chaznedar@edu.cdv.pl>
2023-04-26 10:24:05 -0700

    I created Core Data Database to save news and crypto currencies.

---
+++ CryptoVault/Database/PersistenceController.swift
@@ -0,0 +1,31 @@
+//
+//  PersistenceController.swift
+//  CryptoVault
+//
+//  Created by Cuma Haznedar on 26/04/2023.
+//
+
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+    let container: NSPersistentContainer
+    
+    var viewContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+
+    
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "CryptoVaultDatabase")
+        if inMemory {
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+}
--- CryptoVault/View/News/details/NewsDetailsViewModel.swift
+++ CryptoVault/View/News/details/NewsDetailsViewModel.swift
@@ -5,11 +5,56 @@
 //  Created by Cuma Haznedar on 24/04/2023.
 //
 
-import Foundation
+import SwiftUI
 import Combine
+import CoreData
 
 class NewsDetailsViewModel : ObservableObject {
     
+    private let viewContext = PersistenceController.shared.viewContext
+
+    let container: NSPersistentContainer
+    @Published var newsDatabase: [NewsDatabase] = []
+    @Published var ccc: [CryptoCurrenciesDB] = []
+
     
+    init() {
+
+        container = NSPersistentContainer(name: "CryptoVaultDatabase")
+
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                print("Container load failed: \(error)")
+            } else {
+                print("Succesfully loaded data!")
+            }
+        }
+
+    }
     
+    func addNewsToCoreData(id: String, title: String, imageUrl: String, source: String, date: String) {
+
+        let news = NewsDatabase(context: viewContext)
+        news.id = id
+        news.title = title
+        news.imageUrl = imageUrl
+        news.source = source
+        news.date = date
+                        
+        saveData()
+
+    }
+    
+    private func saveData() {
+
+        do {
+
+            try container.viewContext.save()
+
+        } catch let error {
+
+            print("Error Saving : \(error)")
+
+        }
+    }
 }
